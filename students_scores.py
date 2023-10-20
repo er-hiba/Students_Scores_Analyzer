592c6a0d32543ab3fd656c708bf52de14a18e192
Hiba ERRAOUI <125480922+er-hiba@users.noreply.github.com>
2023-10-20 21:23:21 +0100

    updated the code

--- students_scores.py
+++ students_scores.py
@@ -1,19 +1,52 @@
 n = int(input("Enter the number of students: "))
 
-students_scores = []
+scores = []
 Sum = 0
+
 for i in range (n):
     x = float(input(f"Enter the score of the student {i+1}: "))
-    students_scores.append(x)
+    scores.append(x)
     Sum += x
 
+scores.sort()
+print("The scores in ascending order:", scores)
+
 average = Sum / n
-print("The average is: ", average)
+print("The average(mean):", average)
 
-List = []
+if n % 2 == 0:
+    median = (scores[n//2] + scores[n//2 - 1]) / 2
+else :
+    median = scores [n//2]
+print("The median:", median)
+
+sd = []                 # sd stands for squared differences
+for x in scores :
+    diff = x - average
+    sd.append(diff ** 2)
+
+sum_sd = 0
+for diff in sd:
+    sum_sd += diff
+
+variance = sum_sd / n   # variance (average of squared differences)
+
+std_dev = variance ** 0.5 # square root of variance
+
+print("The standard deviation:", std_dev)
+
+minimum = scores[0]
+print("The minimum:", minimum)
+
+maximum = scores[n - 1]
+print("The maximum:", maximum)
+
+print("The range:", maximum - minimum)
+
+above_average = []
 for i in range (n):
-    if students_scores[i] > average :
-        List.append(students_scores[i])
+    if scores[i] > average :
+        above_average.append(scores[i])
 
-List.sort()
-print("Scores above average are:", List)
+above_average.sort()
+print("The scores above average:", above_average)
