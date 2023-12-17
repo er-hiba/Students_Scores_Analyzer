b7273216267a2635a17553e5f99124b5789d4dbd
Hiba ERRAOUI <125480922+er-hiba@users.noreply.github.com>
2023-12-17 15:55:26 +0100

    Improved the layout of the UI

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import *
 import statistics
 import matplotlib.pyplot as plt
 
@@ -8,55 +8,70 @@ class StudentScoresApp:
         self.root = root
         self.root.title("Student Scores Analyzer")
 
-        self.info_label = tk.Label(root, text="Enter the scores separated by spaces:")
-        self.info_label.pack()
+        # Frame to hold the main elements
+        self.main_frame = Frame(root)
+        self.main_frame.pack()
 
-        self.scores_entry = tk.Entry(root)
-        self.scores_entry.pack()
+        # Labels and Text Entry for user input
+        self.info_label = Label(self.main_frame, text="Enter the scores separated by a space:", font=("Courier", 12))
+        self.info_label.grid(row=0, column=0, padx=10, pady=10)
 
-        self.calculate_button = tk.Button(root, text="Calculate", command=self.calculate_scores)
-        self.calculate_button.pack()
+        self.scores_entry = tk.Text(self.main_frame, width=50, height=10)
+        self.scores_entry.grid(row=1, column=0, padx=10, pady=10)
 
-        self.result_frame = ttk.Frame(root)
-        self.result_frame.pack()
+        # Button to submit scores for analysis
+        self.submit_button = Button(self.main_frame, text="Submit", command=self.analyze_scores)
+        self.submit_button.grid(row=2, column=0, pady=10)
 
-        self.result_text = tk.Text(self.result_frame, height=10, width=50)
-        self.result_text.pack(side=tk.LEFT)
+        # Label and Text widget to display analysis results
+        self.result_label = Label(self.main_frame, text="Results:", font=("Courier", 12))
+        self.result_label.grid(row=3, column=0, pady=10)
 
-        self.plot_frame = ttk.Frame(self.result_frame)
-        self.plot_frame.pack(side=tk.LEFT)
+        self.result_text = Text(self.main_frame, height=20, width=50)
+        self.result_text.grid(row=4, rowspan=2, column=0, padx=10, pady=10)
+        self.result_text.config(font=("Courier", 12))
 
-    def calculate_scores(self):
-        scores_input = self.scores_entry.get()
-        scores = [float(score) for score in scores_input.split()]
+        # Frame to hold the plot
+        self.plot_frame = Frame(self.main_frame)
+        self.plot_frame.grid(row=5, column=1, pady=10)
 
-        if not scores:
+    def analyze_scores(self):
+        # Retrieve scores inputted by the user
+        scores_input = self.scores_entry.get("1.0", tk.END).strip()
+        if not scores_input:
             self.result_text.delete("1.0", tk.END)
             self.result_text.insert(tk.END, "No scores provided.")
+            for widget in self.plot_frame.winfo_children():
+                widget.destroy()
             return
 
-        scores.sort()  # Sort the scores in ascending order
+        # Convert input string to a list of scores and perform analysis
+        scores_list = list(map(float, scores_input.split()))
+        scores_list.sort()
 
-        result = f"The scores in ascending order: {scores}\n"
-        result += f"The average (mean): {statistics.mean(scores)}\n"
-        result += f"The median: {statistics.median(scores)}\n"
-        result += f"The standard deviation: {statistics.stdev(scores)}\n"
-        result += f"The minimum: {min(scores)}\n"  # Use min() to calculate the minimum
-        result += f"The maximum: {max(scores)}\n"
-        result += f"The range: {max(scores) - min(scores)}\n"
-        result += "The scores above average: " + " ".join([str(score) for score in scores if score > statistics.mean(scores)])
+        # Generate analysis results
+        result = "- The scores in ascending order: \n"+" ".join([str(score) for score in scores_list])
+        result += f"\n\n- The average (mean): {statistics.mean(scores_list)}\n"
+        result += f"- The median: {statistics.median(scores_list)}\n"
+        result += f"- The standard deviation: {statistics.stdev(scores_list)}\n"
+        result += f"- The minimum: {min(scores_list)}\n"
+        result += f"- The maximum: {max(scores_list)}\n"
+        result += f"- The range: {max(scores_list) - min(scores_list)}\n"
+        result += "\n- The scores above average: " + " ".join([str(score) for score in scores_list if score > statistics.mean(scores_list)])
 
+        # Display analysis results in the text widget
         self.result_text.delete("1.0", tk.END)
         self.result_text.insert(tk.END, result)
 
-        self.plot_scores(scores)
+        # Generate and display the plot
+        self.plot_scores(scores_list)
 
     def plot_scores(self, scores):
         # Create a figure and axis
-        plt.figure(figsize=(8, 6))
-        plt.xlabel("Student")
-        plt.ylabel("Score")
-        plt.title("Student Scores")
+        plt.figure(figsize=(4, 4))
+        plt.xlabel("Students")
+        plt.ylabel("Scores")
+        plt.title("Students Scores")
 
         # Use integers as student numbers
         students = list(range(1, len(scores) + 1))
@@ -78,18 +93,22 @@ class StudentScoresApp:
         plt.axhline(min_value, linestyle='--', label="Min", color=colors[len(colors) - 2], alpha=0.7)
         plt.axhline(max_value, linestyle='--', label="Max", color=colors[len(colors) - 1], alpha=0.7)
 
+        # Add legend, grid, and adjust layout
         plt.legend()
         plt.grid(True)
         plt.tight_layout()
 
+        # Save the plot as an image and display it in the Tkinter window
         plt.savefig("student_scores.png")
 
         img = tk.PhotoImage(file="student_scores.png")
         img_label = tk.Label(self.plot_frame, image=img)
         img_label.photo = img
-        img_label.pack()
+        img_label.grid(row=2, column=0, pady=10)
+
 
 def main():
+    # Create the Tkinter root window and start the application
     root = tk.Tk()
     app = StudentScoresApp(root)
     root.mainloop()
